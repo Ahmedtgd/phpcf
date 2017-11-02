020f943f63820d08fbd7ee7f90ca90f02dab013a
Ahmed Tarek <ahmedtarek3885@gmail.com>
2017-11-02 06:38:29 +0200

    Update revision.php

--- revision.php
+++ revision.php
@@ -1,15 +1,12 @@
 <?php-                                                                          
-                     10 if( isset($_POST) && !empty($_POST) && is_array($_POST))                        
-                              9 {                                                                               
-                              8  $fullname =$_POST["fullname"];                                                 
-                            7  $email    =$_POST["email"];                                                    
-                              6  $message  =$_POST["message"];                                                  
-                               5  $ip=getenv("REMOTE_ADDR");                                                     
-                               4  $content  = "you got message! <br> From: $fullname <br>Email:$email<br>He says:$message<br> $ip";
-                               3  $response = mail('ahmedtarek3885@gmail.com', 'New Contact us Message',$content);
-                               2  if ($response === true){echo "your message has been sent!";}else{echo"Nothing submited!";}
-                               1 }                                                                               
-                              12                                                                                 
-                               1                                                                                 
-                               2                                                                                 
-                               3 ?> 
+                      if( isset($_POST) && !empty($_POST) && is_array($_POST))                        
+                              {                                                                               
+                               $fullname =$_POST["fullname"];                                                 
+                              $email    =$_POST["email"];                                                    
+                                $message  =$_POST["message"];                                                  
+                                $ip=getenv("REMOTE_ADDR");                                                     
+                                $content  = "you got message! <br> From: $fullname <br>Email:$email<br>He says:$message<br> $ip";
+                                $response = mail('ahmedtarek3885@gmail.com', 'New Contact us Message',$content);
+                                if ($response === true){echo "your message has been sent!";}else{echo"Nothing submited!";}
+                                }                                                                               
+                              ?> 
